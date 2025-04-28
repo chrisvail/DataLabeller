a9966819c72417ec74f2d79d9594bcb040227438
chrisvail <christopher.n.vail@gmail.com>
2025-04-28 11:40:48 +0100

    God knows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,37 @@
 <script lang="ts">
-	import Scene from "$lib/components/threlte/scene.svelte";
-import { Canvas } from "@threlte/core";
-	import type { Matrix4 } from "three";
+	import Scene from '$lib/components/threlte/scene.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { Canvas } from '@threlte/core';
+	import type { Matrix4 } from 'three';
+    import { open } from '@tauri-apps/plugin-dialog';
 
-    let camera_intrinsic_matrix: Matrix4;
-    let camera_extrinsic_matrix: Matrix4;
+	let camera_intrinsic_matrix: Matrix4;
+	let camera_extrinsic_matrix: Matrix4;
+
+    const loadDirectory = async () => {
+        const selected = await open({
+            multiple: false,
+            directory: true,
+            filters: [
+                {
+                    name: 'JSON',
+                    extensions: ['json']
+                }
+            ]
+        });
+
+        console.log(selected);
+    };
 
 </script>
 
 <Canvas>
-    <Scene bind:camera_intrinsic_matrix bind:camera_extrinsic_matrix/>
+	<Scene bind:camera_intrinsic_matrix bind:camera_extrinsic_matrix />
 </Canvas>
-<p class="text-black text-2xl font-bold font-sans">{JSON.stringify(camera_intrinsic_matrix)}</p>
+<div class="flex flex-row">
+    <Button variant="outline" onclick={() => loadDirectory()}>
+        Load Files
+    </Button>
+</div>
+<p class="font-sans text-2xl font-bold text-black">{JSON.stringify(camera_intrinsic_matrix)}</p>
 <p>{JSON.stringify(camera_extrinsic_matrix)}</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
+// import { fontFamily } from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
-	darkMode: ["class"],
+	// darkMode: ["class"],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
-	safelist: ["dark"],
+	// safelist: ["dark"],
 	theme: {
 		container: {
 			center: true,
@@ -66,9 +66,9 @@ const config: Config = {
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)"
 			},
-			fontFamily: {
-				sans: [...fontFamily.sans]
-			},
+			// fontFamily: {
+			// 	sans: [...fontFamily.sans]
+			// },
 			keyframes: {
 				"accordion-down": {
 					from: { height: "0" },
